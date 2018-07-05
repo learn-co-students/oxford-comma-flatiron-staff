6a926bf8de21014815baf5f37eec411673fe150c
Emily Jennings <emily.jennings@flatironschool.com>
2018-07-05 20:26:36 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    string = "#{array[0..1].join(", ")}" + ", and #{array.last}"
+    string
+  else array.length > 3
+    string = "#{array[0..-2].join(", ")}" + ", and #{array.last}"
+    string
+  end
 end
