ab58d66e73d0a61eb98bf57f7bf291bc7c9a2ef5
Thu Do <thu.do@flatironschool.com>
2019-11-26 04:07:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if array.length == 0
+    return ""
+  elsif array.length == 1 
+    return array[0]
+  elsif array.length == 2
+    array.insert(1, "and")
+    return array.join(" ")
+  else array.length > 2 
+          # "a" "b" "c"
+          # "a" "b" "and c" 
+          # a, b, and c
+    array[-1] = "and " << array[-1] 
+    return array.join(", ")
+  end  
 end
