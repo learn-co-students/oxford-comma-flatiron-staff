b59cbeee90b16e0c03302a5820f7a0579d36f70e
Anna Lux <anna.watt@flatironschool.com>
2020-04-16 12:18:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    last_word = array.pop
+    array << "and"
+    array << last_word
+    array.join(" ")
+  else
+    last_word = array.pop
+    array << "and"
+    oxford_string = array.join(", ")
+    oxford_string << " #{last_word}"
+  end
 end
